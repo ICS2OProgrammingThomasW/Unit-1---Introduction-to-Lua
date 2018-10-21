7f0b15cf007f7359ac0cab866e545ce0c7f09dec
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-10-21 14:50:03 -0400

    finished LivesandTimers

---
+++ LivesandTimersThomasW/main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+-- Title: LivesandTimers
+-- Name: Thomas Wehbi
+-- Course: ICS2O
+-- This program will give you a countdown to answer a question, if time runs out you loose a
+-- life you have 4 lifes, if you answer wrong you loose a life etc.
+
+-- variables for the timer
+local totalSeconds = 5
+local secondsLeft = 5
+local ClockText
+local countdownTimer
