1b561a576440c938c0592b3591f38652ecee67c9
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-10-26 13:53:38 -0400

    22

---
+++ WhackAMoleThomasW/main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+-- WhackAMole
+-- Name: Thomas Wehbi
+-- Course: ICS2O
+-- This program places a random object on the screen. If the user clicks on it in
+-- time thye score increases by 1.
+
+
+
+-- Hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+display.setDefault("background", 100/255, 30/255, 50/255)
