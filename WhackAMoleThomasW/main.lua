48d7706ab5cfe36a9de9bab8c9d03b969be1e19c
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-10-28 19:37:01 -0400

    whack a mole

--- WhackAMoleThomasW/main.lua
+++ WhackAMoleThomasW/main.lua
@@ -12,8 +12,30 @@ display.setStatusBar(display.HiddenStatusBar)
 
 display.setDefault("background", 220/255, 195/255, 225/255)
 
+--creating a mole
+local mole = display.newImage( "Images/mole.png" , 0, 0 )
+
 -- setting position
-bkg.anchorX = 0
-bkg.anchorY = 0
+mole.x = display.contentCenterX
+mole.y = display.contentCenterY
+
+-------------------------------------------------------FUNCTIONS-------------------------------------------------------------------
+
+-- this functionthat mkaes the mole appear in a random (x,y) position on the screen 
+-- before calling the Hide function
+function PopUp()
+
+	--choosing random position on the screen between 0 and the size of the screen
+	mole.x = math.random( 0, display.contentWidth )
+	mole.y = math.random( 0, display.contentHeight )
+end
+
+--This function calls the PopUp function after 3 seconds
+function PopUpDelay()
+	timer.performWithDelay( 3000, PopUp)
+end
+
+
+
 
 
