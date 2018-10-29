6ff5df3f8798cd480fdaabcda522ada32c9f6b57
ThomasW2004 <43419344+ThomasW2004@users.noreply.github.com>
2018-10-29 11:48:05 -0400

    whack a mole

--- WhackAMoleThomasW/main.lua
+++ WhackAMoleThomasW/main.lua
@@ -13,11 +13,12 @@ display.setStatusBar(display.HiddenStatusBar)
 display.setDefault("background", 220/255, 195/255, 225/255)
 
 --creating a mole
-local mole = display.newImage( "Images/mole.png" , 0, 0 )
+local Mouse = display.newImage( "Images/Mouse.png" , 0, 0 )
+Mouse:scale(0.1, 0.1)
 
 -- setting position
-mole.x = display.contentCenterX
-mole.y = display.contentCenterY
+Mouse.x = display.contentCenterX
+Mouse.y = display.contentCenterY
 
 -------------------------------------------------------FUNCTIONS-------------------------------------------------------------------
 
@@ -26,14 +27,50 @@ mole.y = display.contentCenterY
 function PopUp()
 
 	--choosing random position on the screen between 0 and the size of the screen
-	mole.x = math.random( 0, display.contentWidth )
-	mole.y = math.random( 0, display.contentHeight )
+	Mouse.x = math.random( 0, display.contentWidth )
+	Mouse.y = math.random( 0, display.contentHeight )
 end
 
---This function calls the PopUp function after 3 seconds
+--This function calls the PopUp function after 3 seconds 
 function PopUpDelay()
-	timer.performWithDelay( 3000, PopUp)
+	timer.performWithDelay( 4000, PopUp)
 end
+
+-- this function makes the mole invisible and then calls the PopUpDelay function 
+function Hide( )
+
+
+	-- changing visiblity
+	Mouse.isVisible = false
+end
+
+-- this function starts the game 
+function GameStart( )
+	PopUpDelay()
+end 
+
+--this function increments the score only if the mole is clicked. it then displays 
+-- the new score.
+function Whacked( event )
+
+	-- if touch phase just started 
+	if (event.phase == "began") then 
+	end 
+
+end 
+
+-------------------------------EVENT LISTENERS---------------------------------
+-- I add the event listener to the moles so that if the mole is touched, the Whacked 
+-- function is called 
+Mouse:addEventListener( "touch", Whacked )
+
+
+
+-------------------------------START THE GAME------------------------------------
+-- starting the game
+function StartGame()
+end 
+
 
 
 
